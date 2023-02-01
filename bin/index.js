40aa2ae501063ae529e2268614d0a8b5164df6c5
pupee <aungchanminmarch@gmail.com>
2023-02-01 23:06:49 +0630

    finished day1

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
-
+const log = console.log;
 const http = require('http');
+const url = require("url");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  const pathname = url.parse(req.url).pathname;
+
+  const module_name = process.env.PWD + pathname;
+  try {
+    const a = require(module_name);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(pathname);
+  } catch {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("such file does not exists");
+  }
 });
 
 server.listen(port, hostname, () => {
